1048e8eb3b0a7ad28f7226a96221859bb99ba959
Will <williameaster@hotmail.com>
2017-12-23 02:50:07 +0000

    Working

---
+++ src/com/swiftpenguin/Factions.java
@@ -0,0 +1,9 @@
+package com.swiftpenguin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class Factions implements Listener {
+
+
+}
---
+++ src/com/swiftpenguin/GriefPrevention.java
@@ -0,0 +1,15 @@
+package com.swiftpenguin;
+
+import org.bukkit.event.Listener;
+
+public class GriefPrevention implements Listener {
+
+    private FlyTime plugin;
+
+    public GriefPrevention(FlyTime plugin) {
+        this.plugin = plugin;
+    }
+
+}
+
+
