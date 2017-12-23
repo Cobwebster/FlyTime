79c1bcf3f313205e6b7c665e8fd43160f094e0fd
Will <williameaster@hotmail.com>
2017-12-23 03:02:00 +0000

    Updates

--- src/com/swiftpenguin/Factions.java
+++ src/com/swiftpenguin/Factions.java
@@ -5,13 +5,5 @@ import org.bukkit.event.Listener;
 
 public class Factions implements Listener {
 
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-
-public class Factions implements Listener {
-
-
-
-}
 
 }
